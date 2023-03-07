30c1a958b4741043f88d1a9cf6103ff96d065fe8
chequangnhat <nhatche42@gmail.com>
2023-03-07 21:44:58 +0700

    add card item

--- src/components/TrendItem.vue
+++ src/components/TrendItem.vue
@@ -1,9 +1,13 @@
 <template lang="">
-  <div
-    class="bg-white w100 border-top border-secondary-subtle column-content-trend"
-  >
-    h
+
+  <div class="card m-0 rounded-0 ">
+  <div class="card-body">
+    <blockquote class="blockquote">
+      <p>A well-known quote, contained in a blockquote element.</p>
+      <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+    </blockquote>
   </div>
+</div>
 </template>
 <script>
 export default {};
