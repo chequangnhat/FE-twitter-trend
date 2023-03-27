f53ad16d6b22db88b846cf8a7c128a5897a8c65d
chequangnhat <nhatche42@gmail.com>
2023-03-28 06:43:50 +0700

    m

--- src/pages/UserInformation.vue
+++ src/pages/UserInformation.vue
@@ -1,17 +1,17 @@
 <script setup>
-import { useTrendsStore } from "../stores/trends";
 import { useFavoriteTrendStore } from "../stores/favorite_trend";
-const favorite_store = useFavoriteTrendStore();
+import TrendItem from "../components/TrendItem.vue";
 
-const trend_store = useTrendsStore()
+const favorite_store = useFavoriteTrendStore();
 </script>
 
 <template>
-  <div class="user-infor-container">
-    user infor
-  </div>
-  <div>
-    liked:
-    {{ favorite_store.favorite_trends }}
+  <div class="trend-detail-container w-4/5 mx-auto my-5 bg-white border pt-3">
+    <div class="text-4xl flex items-center justify-center mb-5">Favorite trend</div>
+    <TrendItem
+      v-for="(trend, index) in favorite_store.favorite_trends"
+      :trend_index="index"
+      :trend="trend"
+    ></TrendItem>
   </div>
 </template>
